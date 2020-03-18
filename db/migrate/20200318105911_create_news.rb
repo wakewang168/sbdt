ae66efa805d786c99702123857f1265d25b6d000
Ellen Wendelin <ellenwendelin@Ellens-MacBook-Air.local>
2020-03-19 04:27:48 +0800

    lacales

--- db/migrate/20200318105911_create_news.rb
+++ db/migrate/20200318105911_create_news.rb
@@ -2,6 +2,8 @@ class CreateNews < ActiveRecord::Migration[6.0]
   def change
     create_table :news do |t|
       t.string :title
+      t.text :body
+      t.datetime :date
 
       t.timestamps
     end
