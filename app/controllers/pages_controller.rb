abcf05cd4ea80ae0af3d806fadacd9f4cb7eed28
Ellen Wendelin <ellenwendelin@Ellens-MacBook-Air.local>
2020-04-27 15:04:08 +0800

    changed the footer and added mq, chnged picture for images etc

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,17 @@
 class PagesController < ApplicationController
   def home
+ #  	@page_title = ‘Index’
+	# @page_description = ‘Senodia Technologies Home page.’
+	# @page_keywords = ‘Senodia, IMU, Inertial Sensors, Accelerometer, Gyroscopes, IC design, ’
   end
+ #  def news
+ #  	@page_title = ‘News’
+	# @page_description = ‘Senodia Technologies Latest News.’
+	# @page_keywords = ‘Senodia, IMU, Inertial Sensors, Accelerometer, Gyroscopes, IC design, News’
+ #  end
+ #  def products
+ #  	@page_title = ‘Index’
+	# @page_description = ‘Senodia Technologies Poducts.’
+	# @page_keywords = ‘Senodia, IMU, Inertial Sensors, Accelerometer, Gyroscopes, IC design, Algorithms’
+ #  end
 end
